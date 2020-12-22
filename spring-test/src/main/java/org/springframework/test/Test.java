94ad0e49a8ebb70f5c881e9930d8c93e40e080f4
Hatake <838149033@qq.com>
2020-12-22 10:23:53 +0800

    first commit

---
+++ spring-test/src/main/java/org/springframework/test/Test.java
@@ -0,0 +1,13 @@
+package org.springframework.test;
+
+/**
+ * @program: spring-framework-master
+ * @description:
+ * @author: Hatake
+ * @create: 2020-06-24 17:42
+ **/
+public class Test {
+	public static void main(String[] args) {
+		System.out.println("123");
+	}
+}
